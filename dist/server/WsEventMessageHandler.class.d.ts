fe80dfc07681a356e2ceccec422ed0f4610c5d24
Acker Apple <y>
2021-08-19 12:58:03 -0400

    update

--- dist/server/WsEventMessageHandler.class.d.ts
+++
@@ -1,5 +0,0 @@
-import { WsMessage } from "../shared/types";
-import { WsEventProcessor } from "./WsEventProcessor.class";
-export declare class WsEventMessageHandler extends WsEventProcessor {
-    processWsEventMessage(data: WsMessage): Promise<void>;
-}
--- dist/server/wss.d.ts
+++
@@ -1,7 +0,0 @@
-import { WsMessage } from "../shared/types";
-import { WsEventMessageHandler } from "./WsEventMessageHandler.class";
-export declare class WsPinConnectionSwitch extends WsEventMessageHandler {
-    setPins(data: WsMessage): void;
-    getPins(data: WsMessage): void;
-    command(data: WsMessage): Promise<void>;
-}
